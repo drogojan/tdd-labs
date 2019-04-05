2ec1ca7c34cf2a5797ca17728dc295969445d38e
Dragos Rogojan <dragos.rogojan@gmail.com>
2019-04-05 15:36:02 +0300

    Add project files.

---
+++ SimpleMocks.tests/LoginManager1UnitTests.cs
@@ -0,0 +1,44 @@
+﻿using MyBillingProduct;
+using NUnit.Framework;
+
+namespace SimpleMocks.tests
+{
+    [TestFixture]
+    public class LoginManager1UnitTests
+    {
+        [TestCase("abc", "abc", "login ok: user: abc")]
+        [TestCase("abcd", "abcd", "login ok: user: abcd")]
+        public void IsLoginOK_WithExistingUser_CallLogger(string username, string password, string expectedMessage)
+        {
+            FakeLogger stubLogger = new FakeLogger();
+            LoginManager1 loginManager1 = new LoginManager1(stubLogger);
+            loginManager1.AddUser(username, password);
+
+            bool logingResult = loginManager1.IsLoginOK(username, password);
+
+            Assert.AreEqual(expectedMessage, stubLogger.Written);
+        }
+
+        [TestCase("abc", "abc", "bad login: abc,abc")]
+        [TestCase("abcd", "abcd", "bad login: abcd,abcd")]
+        public void IsLoginOK_WithNonExistingUser_CallLogger(string username, string password, string expectedMessage)
+        {
+            FakeLogger stubLogger = new FakeLogger();
+            LoginManager1 loginManager1 = new LoginManager1(stubLogger);
+
+            bool logingResult = loginManager1.IsLoginOK(username, password);
+
+            Assert.AreEqual(expectedMessage, stubLogger.Written);
+        }
+    }
+
+    public class FakeLogger : ILogger
+    {
+        public void Write(string text)
+        {
+            Written = text;
+        }
+
+        public string Written { get; private set; }
+    }
+}
---
+++ SimpleMocks/LoginManager1.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+
+namespace MyBillingProduct
+{
+	public class LoginManager1
+	{
+	    private Hashtable m_users = new Hashtable();
+        private ILogger logger;
+
+        public LoginManager1(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public bool IsLoginOK(string user, string password)
+	    {
+            if (m_users[user] != null &&
+	            m_users[user] == password)
+	        {
+                logger.Write($"login ok: user: {user}");
+	            return true;
+	        }
+            logger.Write($"bad login: {user},{password}");
+	        return false;
+	    }
+
+	    public void AddUser(string user, string password)
+	    {
+	        m_users[user] = password;
+	    }
+
+	    public void ChangePass(string user, string oldPass, string newPassword)
+		{
+			m_users[user]= newPassword;
+		}
+	}
+}
